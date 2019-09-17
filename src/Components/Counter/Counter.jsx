fb8521f928b36c024eacfec710af4c29f10088da
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-17 18:25:09 +0200

    show component counter when tasks list is at 0

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import "./Counter.css";
 
-const Counter = ({ number }) => <div className="counter">{number}</div>;
+const Counter = ({ number }) => (
+    <div className="counter" hidden={number > 0 ? false : true}>
+        {number}
+    </div>
+);
 
 export default Counter;
